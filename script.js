53263d69b416184c2c3b31c4552d1462c4f5c154
eatplus2024 <edwincito2021@gmail.com>
2025-04-03 16:31:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ const images = [
     {
         id: 4,
         url: "https://static.wixstatic.com/media/a4f6c8_af96e3a54c2140f0a1104de0c8c6b093~mv2.png/v1/fill/w_720,h_360,al_c,lg_1,q_85,enc_avif,quality_auto/a4f6c8_af96e3a54c2140f0a1104de0c8c6b093~mv2.png",
-        keywords: ["donde pacho", "ropa deportiva", "prendas", "camisetas deportivas", "sudaderas", "pantalonetas", "balones", "peluches", "accesorios", "deportes"],
+        keywords: ["variedades donde pacho", "ropa deportiva", "prendas", "camisetas deportivas", "sudaderas", "pantalonetas", "balones", "peluches", "accesorios", "deportes"],
         link: "https://eatcomercial01.wixsite.com/website-1/copy-2-of-locales-6/locales",
     },
     {
@@ -264,7 +264,7 @@ const images = [
     {
         id: 4,
         url: "https://static.wixstatic.com/media/a4f6c8_9f323f52c2c54f049a458c117f20b8af~mv2.png/v1/fill/w_176,h_152,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/a4f6c8_9f323f52c2c54f049a458c117f20b8af~mv2.png",
-        keywords: ["la talanquera del llano", "sombrearos", "ponchos", "llaneridades", "artesanias", "gorras", "correas", "llaveros", "cotizas", "chimu", "estuches en cuero para celular", "grasa de potro", "aceite de palma", "aceite de palo", "cuchillos", "hamacas"],
+        keywords: ["la talanquera del llano", "sombreros", "ponchos", "llaneridades", "artesanias", "gorras", "correas", "llaveros", "cotizas", "chimu", "estuches en cuero para celular", "grasa de potro", "aceite de palma", "aceite de palo", "cuchillos", "hamacas"],
         link: "https://eatcomercial01.wixsite.com/website-1/copy-3-of-locales-8/locales",
     },
     {
